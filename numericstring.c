5f78142587e57420810256a6805cf9dc3dc09414
manisu <35994839+manisu@users.noreply.github.com>
2018-02-07 16:23:25 +0530

    numerical string
    program

---
+++ numericstring.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char a[50];
+printf("\n enter the string");
+scanf("%s",a);
+if(0<a)
+ printf("\n yes");
+else 
+ printf("\n no");
+getch();
+}
