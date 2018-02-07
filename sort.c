42c830d4abb529dff6d54bfbe87f61e56a55d93f
manisu <35994839+manisu@users.noreply.github.com>
2018-02-07 16:07:18 +0530

    Create sort.c

---
+++ sort.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+int array[100],i,largest,size,j,temp;
+printf("\n enter the size");
+scanf("%d",&size);
+for(i=0;i<size;i++)
+{
+scanf("%d",&array[i]);
+}
+largest=array[0];
+for(i=0;i<size;i++)
+{
+if(largest<array[i])
+{   largest=array[i];
+}
+printf("%d",largest);
+}
+getch();
+}
+
