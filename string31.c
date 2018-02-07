ff9369da5a38d89f0ce5f03738ed849363df4e16
manisu <35994839+manisu@users.noreply.github.com>
2018-02-07 16:54:15 +0530

    Create string31.c

---
+++ string31.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char a[100];
+int string_length;
+printf("\n enter the string");
+scanf("%s",a);
+string_length=strlen(a);
+printf("\n string length=%d",string_length);
+getch();
+}
