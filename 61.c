7543bfdfe04a70c96d903718f18d1c7a36ff66cf
manisu <35994839+manisu@users.noreply.github.com>
2018-02-23 16:35:39 +0530

    Create 61.c

---
+++ 61.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+int a[100],i,n;
+printf("\n enter the string");
+for(i=0;i<n;i++)
+{
+scanf("%c",&a[i]);
+}
+printf("\n enter the number ");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+printf("%c",a[i]);
+}
+getch();
+}
