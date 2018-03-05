cba251525ccfc67bc38cbbb90733512213d3e28a
manisu <35994839+manisu@users.noreply.github.com>
2018-03-05 16:52:12 +0530

    Create reverse97.c

---
+++ reverse97.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num,d,r;
+printf("\n Enter the number");
+scanf("%d",&num);
+while(num!=0)
+{
+d=num%10;
+num=num/10;
+r=r*10+digit;
+printf("\n Reverse of a number is :%d",r);
+}
+getch();
+}
